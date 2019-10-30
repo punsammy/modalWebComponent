0a4e4ff52bfdf832feca6c59437c5eb5feb8ab0e
unknown <amandapunsammy@xennialconsulting.com>
2019-10-30 02:11:06 -0400

    add attribteChangedCallback to open modal

--- modal.js
+++ modal.js
@@ -5,6 +5,23 @@ class Modal extends HTMLElement {
     const template = document.querySelector("#wc-modal");
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
+  
+  attributeChangedCallback(name, oldValue, newValue){
+    if (name === "opened") {
+      if (this.hasAttribute("opened")) {
+        this.shadowRoot.querySelector("#backdrop").style.opacity = 1;
+        this.shadowRoot.querySelector("#backdrop").style.pointerEvents = "all";
+        this.shadowRoot.querySelector("#modal").style.opacity = 1;
+        this.shadowRoot.querySelector("#modal").style.pointerEvents = "all";
+
+      }
+    }
+  }
+
+  static get observedAttributes(){
+    return ["opened"];
+  }
 }
+
 
 customElements.define("basic-modal", Modal);
